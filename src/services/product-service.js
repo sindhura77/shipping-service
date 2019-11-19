debf6b3af1544e7fe3d4e1f138b8da737a4e829c
sindhura77 <45347991+sindhura77@users.noreply.github.com>
2019-11-19 14:30:32 +0530

    initial commit

---
+++ src/services/product-service.js
@@ -0,0 +1,18 @@
+// src/services/product-service.js
+var axios = require('axios')
+module.exports = {
+getProductWeight: async function (productId) {
+return axios
+.get('https://product.service:8899/products/' + productId)
+.then(response => {
+if (response.data && !Number.isNaN(parseFloat(response.data.weightLB))) {
+return response.data.weightLB
+} else {
+return Promise.reject('Invalid response object')
+}
+})
+.catch( (err) => {
+throw new Error(err)
+})
+}
+}
